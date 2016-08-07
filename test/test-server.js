6a0a8f533be8a371b7f5c10952c12f95ea452f5e
Yonathan Ayenew <yonayen@gmail.com>
2016-08-07 16:34:53 -0700

    add testing suite to app

---
+++ test/test-server.js
@@ -0,0 +1,12 @@
+describe ('Shoppping List', function() {
+	it('POST to an ID that exists');
+	it('POST without body data');
+	it('POST with something other than valid JSON');
+	it('PUT without an ID in the endpoint');
+	it('PUT with different ID in the endpoint than the body');
+	it("PUT to an ID that doesn't exist");
+	it("PUT without body data");
+	it("PUT with something other than valid JSON");
+	it("DELETE an ID that doesn't exist");
+	it("DELETE without an ID in the endpoint");
+});
