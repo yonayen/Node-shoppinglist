e983175c7610fa6c96c6ce6ecec4065704f83c78
Yonathan Ayenew <yonayen@gmail.com>
2016-08-10 17:47:38 -0700

    fix last test errors

--- test/test-server.js
+++ test/test-server.js
@@ -1,9 +1,11 @@
 describe ('Shoppping List', function() {
 	it('POST to an ID that exists');
 	it('POST without body data');
-	
+
 	it('POST with something other than valid JSON');
+
 	it('PUT without an ID in the endpoint');
+	
 	it('PUT with different ID in the endpoint than the body');
 	it("PUT to an ID that doesn't exist");
 	it("PUT without body data");
