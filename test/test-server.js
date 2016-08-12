22785179f286252e817fd99d4985bcf3918f5cb2
Yonathan Ayenew <yonayen@gmail.com>
2016-08-12 16:59:55 -0700

    add chai testing suite

--- test/test-server.js
+++ test/test-server.js
@@ -1,4 +1,27 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
+chai.use(chaiHttp);
+
 describe ('Shoppping List', function() {
+	it('should list items on GET', function(done) {
+        chai.request(app)
+            .get('/items')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                console.log(res);
+                res.should.be.json;
+                done();
+            });
+    });
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
 	it('POST to an ID that exists');
 	it('POST without body data');
 	it('POST with something other than valid JSON');
@@ -9,4 +32,6 @@ describe ('Shoppping List', function() {
 	it("PUT with something other than valid JSON");
 	it("DELETE an ID that doesn't exist");
 	it("DELETE without an ID in the endpoint");
+
+
 });
